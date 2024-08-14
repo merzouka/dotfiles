3acec3244e224900c230de76f242b95289633e1e
merzouka <marzoukayouness@gmail.com>
2024-08-14 19:37:46 +0100

    removed redundant config

--- fish/config.fish
+++ fish/config.fish
@@ -11,3 +11,10 @@ alias cd=z
 
 # set up nvim as editor
 set -gx EDITOR nvim
+
+# pnpm
+set -gx PNPM_HOME "/home/merzouka/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
