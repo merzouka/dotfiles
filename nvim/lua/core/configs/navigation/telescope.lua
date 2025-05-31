cf9f001ed9fa80538d03bc7f7a7aa42b87992bc6
merzouka <marzoukayouness@gmail.com>
2025-05-31 06:16:16 +0100

    Fix: Removed flutter

--- nvim/lua/core/configs/navigation/telescope.lua
+++ nvim/lua/core/configs/navigation/telescope.lua
@@ -13,5 +13,3 @@ vim.keymap.set('n', '<leader>hs', builtin.help_tags, { desc = '[H]elp [S]earch' 
 
 -- telescope fzf native
 pcall(require('telescope').load_extension, 'fzf')
--- flutter
-require("telescope").load_extension("flutter")
--- nvim/lua/core/configs/special/init.lua
+++ nvim/lua/core/configs/special/init.lua
@@ -1,3 +1,2 @@
 require("core.configs.special.obsidian")
 require("core.configs.special.tex")
-require("core.configs.special.flutter")
