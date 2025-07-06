81556d7887cc6c9a1767be595d81aa27273affee
merzouka <marzoukayouness@gmail.com>
2025-07-06 19:36:27 +0100

    Restored Lua Line

--- nvim/lua/core/configs/ui/lualine.lua
+++ nvim/lua/core/configs/ui/lualine.lua
@@ -1,9 +1,8 @@
--- require("lualine").setup({
---     options = {
---         icons_enabled = true,
---         -- section_separators = { left = '', right = '' },
---         -- component_separators = { left = '|', right = '|' },
---         theme = 'auto',
---     }
--- })
-
+require("lualine").setup({
+    options = {
+        icons_enabled = true,
+        -- section_separators = { left = '', right = '' },
+        -- component_separators = { left = '|', right = '|' },
+        theme = 'auto',
+    }
+})
