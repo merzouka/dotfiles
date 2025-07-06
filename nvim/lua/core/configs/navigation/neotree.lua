3d5db3a55b990d4cbba7007c568bcfac83cf5150
merzouka <marzoukayouness@gmail.com>
2025-07-06 19:34:53 +0100

    Uncommented neotree configuration

--- nvim/lua/core/configs/navigation/neotree.lua
+++ nvim/lua/core/configs/navigation/neotree.lua
@@ -1,31 +1,31 @@
---
--- -- keymaps
--- vim.keymap.set('n', '<C-n>', ':Neotree toggle reveal_force_cwd<cr>', { desc = '[N]eotree Toggle' })
---
--- -- configuration
--- -- Note: only listed useful mappings
--- require('neo-tree').setup({
---     mappings = {
---         ["P"] = { "toggle_preview", config = { use_float = true } },
---         ["l"] = "focus_preview",
---         ["r"] = "rename",
---         ["d"] = "delete",
---         ["s"] = "open_split",
---         ["v"] = "open_vsplit",
---         ["y"] = "copy_to_clipboard",
---         ["x"] = "cut_to_clipboard",
---         ["p"] = "paste_to_clipboard",
---     },
---     window = {
---         mappings = {
---             ["/"] = "fuzzy_finder",
---         },
---         fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
---             ["<down>"] = "move_cursor_down",
---             ["<C-n>"] = "move_cursor_down",
---             ["<up>"] = "move_cursor_up",
---             ["<C-p>"] = "move_cursor_up",
---         },
---     },
---
--- })
+
+-- keymaps
+vim.keymap.set('n', '<C-n>', ':Neotree toggle reveal_force_cwd<cr>', { desc = '[N]eotree Toggle' })
+
+-- configuration
+-- Note: only listed useful mappings
+require('neo-tree').setup({
+    mappings = {
+        ["P"] = { "toggle_preview", config = { use_float = true } },
+        ["l"] = "focus_preview",
+        ["r"] = "rename",
+        ["d"] = "delete",
+        ["s"] = "open_split",
+        ["v"] = "open_vsplit",
+        ["y"] = "copy_to_clipboard",
+        ["x"] = "cut_to_clipboard",
+        ["p"] = "paste_to_clipboard",
+    },
+    window = {
+        mappings = {
+            ["/"] = "fuzzy_finder",
+        },
+        fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+            ["<down>"] = "move_cursor_down",
+            ["<C-n>"] = "move_cursor_down",
+            ["<up>"] = "move_cursor_up",
+            ["<C-p>"] = "move_cursor_up",
+        },
+    },
+
+})
