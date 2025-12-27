f476ec31c05a13ec1c56ab570576d897addbeeff
merzouka <marzoukayouness@gmail.com>
2025-12-27 21:47:53 +0100

    feat(nvim): Added json 2 tab space

---
+++ nvim/lua/core/configs/lang-specific/json.lua
@@ -0,0 +1,12 @@
+-- local function ingipath(pattern)
+--     return vim.fn.expand("~") .. "/code/inge/website/src/**/" .. pattern
+-- end
+
+vim.api.nvim_create_autocmd({"BufEnter"}, {
+    pattern = {"*.json"},
+    callback = function ()
+        vim.opt.tabstop = 2
+        vim.opt.shiftwidth = 2
+        vim.opt.softtabstop = 2
+    end
+})
