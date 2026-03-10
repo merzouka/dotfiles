b0c3a3a06ee7762c4e65cf174b054d86bab2aa7e
merzouka <marzoukayouness@gmail.com>
2026-03-10 10:45:21 +0100

    feat(nvim): Added rust autoclose of closures

--- nvim/lua/core/configs/editing/basic.lua
+++ nvim/lua/core/configs/editing/basic.lua
@@ -7,6 +7,9 @@ require("nvim-surround").setup()
 require("nvim-ts-autotag").setup()
 
 require("autoclose").setup({
+   keys = {
+      ["|"] = { escape = true, close = true, pair = "||", enabled_filetypes = {"rust"} },
+   },
     options = {
         auto_indent = true,
         pair_spaces = false,
